ba1e4c6c1644b6f5d343e1c5496255c974ea29a3
Vi Tiet <vitiet.programmer@gmail.com>
2022-11-20 18:17:43 -0500

    Initial Commit

---
+++ Assets/Scripts/NoiseDeform.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class NoiseDeform : MonoBehaviour
+{
+    [SerializeField] private float NoiseScale = 0.5f;
+    [SerializeField] private float speed = 0.8f;
+    [SerializeField] private bool recalculateNormals = false;
+
+    private Vector3[] baseVertices;
+    private Perlin noise;
+    private GameObject oscReceiverGO;
+
+    private void Awake()
+    {
+        oscReceiverGO = GameObject.Find("OSC Receiver");
+    }
+
+    private void Start()
+    {
+        noise = new Perlin();
+    }
+
+    private void Update()
+    {
+        var mesh = GetComponent<MeshFilter>().mesh;
+
+        var intensity = oscReceiverGO.GetComponent<OSCReceiver>().NoiseIntensity;
+
+        var scale = NoiseScale * intensity;
+
+        if (baseVertices == null)
+            baseVertices = mesh.vertices;
+
+        var vertices = new Vector3[baseVertices.Length];
+
+        var timex = Time.time * speed + 0.1365143f;
+        var timey = Time.time * speed + 1.21688f;
+        var timez = Time.time * speed + 2.5564f;
+        for (var i = 0; i < vertices.Length; i++)
+        {
+            var vertex = baseVertices[i];
+
+            vertex.x += noise.Noise(timex + vertex.x, timex + vertex.y, timex + vertex.z) * scale;
+            vertex.y += noise.Noise(timey + vertex.x, timey + vertex.y, timey + vertex.z) * scale;
+            vertex.z += noise.Noise(timez + vertex.x, timez + vertex.y, timez + vertex.z) * scale;
+
+            vertices[i] = vertex;
+        }
+
+        mesh.vertices = vertices;
+
+        if (recalculateNormals)
+            mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+    }
+}
---
+++ Assets/Scripts/OSCReceiver.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class OSCReceiver : MonoBehaviour
+{
+    public string RemoteIP = "127.0.0.1"; //127.0.0.1 signifies a local host (if testing locally
+    public int SendToPort = 5555; //the port you will be sending from
+    public int ListenerPort = 2222; //the port you will be listening on
+    public Transform controller;
+    public float NoiseIntensity => val;
+
+    private float val;
+
+    // the OSC object
+    private Osc osc;
+    private Udp udp;
+
+    private void Start()
+    {
+        udp = GetComponent<Udp>();
+        udp.init(RemoteIP, SendToPort, ListenerPort);
+
+        osc = GetComponent<Osc>();
+        osc.init(udp);
+        osc.SetAllMessageHandler(AllMessageHandler);
+    }
+
+    private void AllMessageHandler(OscMessage msg)
+    {
+        // log the OSC message
+        Debug.Log(Osc.OscMessageToString(msg));
+
+        // message parameters
+        var address = msg.Address;
+        var values = msg.Values;
+
+        // different actions, based on the address pattern
+        switch (address)
+        {
+            // FORMAT:  /cursor id group_id x y z x_world y_world z_world 
+            case "/test":
+                // extract the data
+                val = (float) values[0];
+
+                // log the data
+                Debug.Log("Noise Intensity: " + val);
+                break;
+        }
+    }
+}
